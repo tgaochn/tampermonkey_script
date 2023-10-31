c2bb755019c33dd88910b141656a43a7c245c191
tgaochn <tgaochn@gmail.com>
2023-10-30 23:10:09 -0500

    0.2.0: 优化按钮位置, 优化输入框位置

--- chatGPT_add_buttons/chatGPT_add_buttons.js
+++ chatGPT_add_buttons/chatGPT_add_buttons.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        chatGPT_add_buttons 
 // @namespace   https://chat.openai.com/
-// @version     0.1.8
+// @version     0.2.0
 // @description Adds more buttons for chatGPT; modified from "Add continue button (页面优化) v1.2.1"
 // @author      gtfish
 // @match       https://chat.openai.com/*
@@ -11,6 +11,7 @@
 // @downloadURL     https://raw.githubusercontent.com/tgaochn/tampermonkey_script/master/chatGPT_add_buttons/chatGPT_add_buttons.js
 // ==/UserScript==
 
+// 0.2.0: 优化按钮位置, 优化输入框位置
 // 0.1.8: 修改某些按钮, 现在可以配合 Superpower ChatGPT 插件使用
 // 0.1.6: 增加新按钮
 // 0.1.5: 优化按钮功能, 可以直接send msg
@@ -46,11 +47,18 @@
             return;
         }
 
+
         const buttonContainer = document.createElement('div');
-        buttonContainer.style.display = 'inline';
+        buttonContainer.style.display = 'inline-block';
         buttonContainer.style.justifyContent = 'center';
         buttonContainer.style.marginBottom = '10px';
         buttonContainer.style.marginLeft = '10px';
+
+        const buttonContainer2 = document.createElement('div');
+        buttonContainer2.style.display = 'inline-block';
+        buttonContainer2.style.justifyContent = 'center';
+        buttonContainer2.style.marginBottom = '10px';
+        buttonContainer2.style.marginLeft = '10px';
 
         // button: continue
         buttonContainer.append(
@@ -117,57 +125,59 @@
             })
         );
  
-        // // button: 解释, 英翻中
-        // buttonContainer.append(
-        //     createButton('解释, 英翻中', function () {
-        //         const textArea = document.querySelector('textarea');
-        //         textArea.value = 'What is "XXX". What does the term "XXX" mean in this content. Give me a detailed explanation and some examples in English. Then translate the response into Chinese.';
-        //         textArea.focus();
-        //     })
-        // );
- 
-        // // button: 中翻英
-        // buttonContainer.append(
-        //     createButton('中翻英', function () {
-        //         const textArea = document.querySelector('textarea');
-        //         textArea.value = 'translate the following Chinese text into English in different tones, which will be used in messages between colleagues and formal emails: ';
-        //         textArea.focus();
-        //     })
-        // );
- 
-        // // button: 改写
-        // buttonContainer.append(
-        //     createButton('改写', function () {
-        //         const textArea = document.querySelector('textarea');
-        //         textArea.value = 'Rewrite the following text in different tones, which will be used in project documents, messages between colleagues, and formal emails: ';
-        //         textArea.focus();
-        //     })
-        // );
+        // button: 改写
+        buttonContainer2.append(
+            createButton('改写', function () {
+                const textArea = document.querySelector('textarea');
+                textArea.value = 'Rewrite the following text in different tones, which will be used in project documents, messages between colleagues, and formal emails: \n';
+                textArea.focus();
+            })
+        );
 
-        // // button: 翻译
-        // buttonContainer.append(
-        //     createButton('解释翻译', function () {
-        //         const textArea = document.querySelector('textarea');
-        //         textArea.value = 'For the following test, explain in detail what it means and what it may possibly imply (in English). Then, translate it and do the explanation again in Chinese: ';
-        //         textArea.focus();
-        //     })
-        // );
+        // button: 翻译
+        buttonContainer2.append(
+            createButton('解释翻译', function () {
+                const textArea = document.querySelector('textarea');
+                textArea.value = 'For the following test, explain in detail what it means and what it may possibly imply (in English). Then, translate it and do the explanation again in Chinese:\n ';
+                textArea.focus();
+            })
+        );
 
-        // // button: 总结
-        // buttonContainer.append(
-        //     createButton('总结', function () {
-        //         const textArea = document.querySelector('textarea');
-        //         textArea.value = 'Summarize the following text in both English and Chinese in a paragraph then reformat it in some bullets: ';
-        //         textArea.focus();
-        //     })
-        // );
+        // button: 总结
+        buttonContainer2.append(
+            createButton('总结', function () {
+                const textArea = document.querySelector('textarea');
+                textArea.value = 'Summarize the following text in both English and Chinese in a paragraph then reformat it in some bullets: \n';
+                textArea.focus();
+            })
+        );
+
+        // button: 解释, 英翻中
+        buttonContainer2.append(
+            createButton('解释, 英翻中', function () {
+                const textArea = document.querySelector('textarea');
+                textArea.value = 'What is "XXX". What does it mean in this content. Give me a detailed explanation and some examples in English. Then translate the response into Chinese.';
+                textArea.focus();
+            })
+        );
+ 
+        // button: 中翻英
+        buttonContainer2.append(
+            createButton('中翻英', function () {
+                const textArea = document.querySelector('textarea');
+                textArea.value = 'translate the following Chinese text into English in different tones, which will be used in messages between colleagues and formal emails: \n';
+                textArea.focus();
+            })
+        );
+
 
         textBox.appendChild(buttonContainer);
-        textBox.style.display = 'inline';
+        textBox.style.display = 'inline-block';
+        textBox.parentNode.insertBefore(buttonContainer2, textBox.nextSibling);
 
         const divElements = textBox.querySelectorAll('div');
         for (let i = 0; i < divElements.length; i++) {
-            divElements[i].style.display = 'inline';
+            divElements[i].style.display = 'inline-block';
         }
     }
 
@@ -186,7 +196,7 @@
     function adjustModelSwitcher() {
         const modelSwitcher = document.getElementById('chatgpt-model-switcher');
         if (modelSwitcher) {
-            modelSwitcher.style.display = 'inline';
+            modelSwitcher.style.display = 'inline-block';
             modelSwitcher.style.width = 'auto';
             modelSwitcher.style.marginLeft = '10px';
             return true;
