818f35300867b83fc55a843ff5997bbd3f705315
5800X\gtfish <tgaochn@gmail.com>
2024-02-06 21:59:39 -0600

    update jump script

--- AnyWebsite_jump/aws_region2.js
+++ AnyWebsite_jump/aws_region2.js
@@ -4,10 +4,19 @@
 // @version      0.0.1
 // @description  任意网站跳转 - aws region1 跳转到 region2
 // @author       gtfish
-// @include      *://us-east-1.console.aws.amazon.com/console/home?region=us-east-1#
+// @include      *://*.console.aws.amazon.com/*
+// @include      *
 // @updateURL       https://raw.githubusercontent.com/tgaochn/tampermonkey_script/master/AnyWebsite_jump/aws_region2.js
 // @downloadURL     https://raw.githubusercontent.com/tgaochn/tampermonkey_script/master/AnyWebsite_jump/aws_region2.js
 
 // ==/UserScript==
 
-document.location = "https://us-east-2.console.aws.amazon.com/console/home?region=us-east-2#";
+(function() {
+    'use strict';
+
+    // Check if the current URL is the specific AWS console URL
+    if (window.location.href === "https://us-east-1.console.aws.amazon.com/console/home?region=us-east-1#" || window.location.href === "https://us-east-1.console.aws.amazon.com/console/home?region=us-east-1") {
+        // Redirect to the US East 2 console
+        window.location.href = "https://us-east-2.console.aws.amazon.com/console/home?region=us-east-2#";
+    }
+})();
