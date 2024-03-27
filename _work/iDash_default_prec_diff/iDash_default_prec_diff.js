19aa85e59f23682d08be00c2b45bea77e4b2590c
5800X\gtfish <tgaochn@gmail.com>
2024-03-27 15:29:12 -0500

    Update iDash_default_prec_diff version to 0.0.2 and add hasPercentDiff parameter

--- _work/iDash_default_prec_diff/iDash_default_prec_diff.js
+++ _work/iDash_default_prec_diff/iDash_default_prec_diff.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         iDash_default_prec_diff
 // @namespace    iDash_default_prec_diff
-// @version      0.0.1
+// @version      0.0.2
 // @description  自动开启iDash的precDiff
 // @author       gtfish
 // @include      *://idash.sandbox.indeed.net/queryshare/?query=*
@@ -10,20 +10,14 @@
 
 // ==/UserScript==
 
-// https://idash.sandbox.indeed.net/queryshare/?query=FROM%20rjpFeed(rjpgrp%20%3D~%20%27.*%3Aidxbutterflyapplymodeltst629%27)%202024-02-27%202024-03-13%20AS%20ctrl%2C%0A%20%20%20%20%20rjpFeed(rjpgrp%20%3D~%20%27.*%3Aidxbutterflyapplymodeltst630%27)%20AS%20test1%2C%0A%20%20%20%20%20rjpFeed(rjpgrp%20%3D~%20%27.*%3Aidxbutterflyapplymodeltst631%27)%20AS%20test2%0AWHERE%20%20surfaceId%20in%20(%27hp%27%2C%20%27hpd%27)%20country%3D%27us%27%0AGROUP%20BY%20DATASET()%0ASELECT%0ACOUNT()%2C%0Aoji%2Bsji%2C%20%0Aoji%2Fsji%2C%0Abelowthresholdremovednum%2C%20%0Aoj_threshold_filter_num_filtered%2C%0Asj_threshold_filter_jobs_filtered%0A&engine=IQL2&name=query1&author=virtee&limit=5%2C000&hasElaborations=true&hasPercentDiff=true&listOfParameters=%7B%7D&ddUid=14105fb2-c651-4794-8b4f-5b8186678b86&fromInsightUid=idashblank_653646d8-5a4e-40d5-afdf-4f1a4da0bd9a
-
 (function () {
     'use strict';
 
     const urlObj = new URL(window.location.href);
-    urlObj.searchParams.set("hasPercentDiff", "true");
-    // url = urlObj.toString();
-    window.location.href = urlObj.toString();
+    // 仅在 hasPercentDiff 参数不存在时才添加
+    if (!urlObj.searchParams.has("hasPercentDiff")) {
+        urlObj.searchParams.set("hasPercentDiff", "true");
+        window.history.replaceState({}, "", urlObj.toString());
+    }
 
-    // // Check if the current URL is the specific AWS console URL
-    // if (window.location.href === "https://us-east-1.console.aws.amazon.com/console/home?region=us-east-1#" || window.location.href === "https://us-east-1.console.aws.amazon.com/console/home?region=us-east-1") {
-
-    //     // window.location.href = "https://us-east-2.console.aws.amazon.com/console/home?region=us-east-2#"; // redirect to region2 console
-    //     window.location.href = "https://us-east-2.console.aws.amazon.com/console/home?region=us-east-2#"; // redirect to region2 athena
-    // }
 })();
