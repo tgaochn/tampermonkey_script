558035e6b085d8d50b2917cbffb9815f8f6c9a2d
5800X\gtfish <tgaochn@gmail.com>
2024-03-27 15:56:48 -0500

    Add 'run-at document-start' to Tampermonkey script

--- _work/iDash_default_prec_diff/iDash_default_prec_diff.js
+++ _work/iDash_default_prec_diff/iDash_default_prec_diff.js
@@ -7,17 +7,19 @@
 // @include      *://idash.sandbox.indeed.net/queryshare/?query=*
 // @updateURL       https://raw.githubusercontent.com/tgaochn/tampermonkey_script/master/_work/iDash_default_prec_diff/iDash_default_prec_diff.js
 // @downloadURL     https://raw.githubusercontent.com/tgaochn/tampermonkey_script/master/_work/iDash_default_prec_diff/iDash_default_prec_diff.js
+// @run-at document-start
 
 // ==/UserScript==
+// TODO: still not working
 
 (function () {
     'use strict';
 
     const urlObj = new URL(window.location.href);
+    
     // 仅在 hasPercentDiff 参数不存在时才添加
     if (!urlObj.searchParams.has("hasPercentDiff")) {
         urlObj.searchParams.set("hasPercentDiff", "true");
         window.history.replaceState({}, "", urlObj.toString());
     }
-
 })();
