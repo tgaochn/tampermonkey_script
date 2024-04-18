caa80c66ea95a681ec2e95c92f6501581e3bced9
5800X\gtfish <tgaochn@gmail.com>
2024-04-17 22:46:06 -0500

    Update Butterfly_webapp_btn version to 0.2.4 and add copy build ID and copy hypertext functionality

--- _work/Butterfly_webapp_btn/Butterfly_webapp_btn.js
+++ _work/Butterfly_webapp_btn/Butterfly_webapp_btn.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name                Butterfly_webapp_btn
-// @version             0.2.3
+// @version             0.2.4
 // @description         Add btn on Butterfly webapp
 // @author              gtfish
 // @license             MIT
@@ -12,6 +12,7 @@
 // @updateURL           https://raw.githubusercontent.com/tgaochn/tampermonkey_script/master/_work/Butterfly_webapp_btn/Butterfly_webapp_btn.js
 // @downloadURL         https://raw.githubusercontent.com/tgaochn/tampermonkey_script/master/_work/Butterfly_webapp_btn/Butterfly_webapp_btn.js
 // ==/UserScript==
+// 0.2.4: Added copy build ID and copy hypertext functionality
 // 0.2.0: 增加copy build id
 // 0.1.0: 优化了hypertext的复制逻辑
 // 0.0.1: init, 添加若干按钮
@@ -53,6 +54,36 @@
         return buttonContainer;
     }
 
+    function generateHypertext(text, url, leftPart = '', rightPart = '') {
+        // Create a new anchor element
+        let hyperlinkElem = document.createElement('a');
+        hyperlinkElem.textContent = text;
+        hyperlinkElem.href = url;
+    
+        // 创建一个新的span元素,用于包裹超链接和括号
+        var spanElem = document.createElement('span');
+        spanElem.appendChild(document.createTextNode(leftPart));
+        spanElem.appendChild(hyperlinkElem);
+        spanElem.appendChild(document.createTextNode(rightPart));
+    
+        // 临时将span元素插入到页面中(隐藏不可见), 这样才能选中并复制
+        spanElem.style.position = 'absolute';
+        spanElem.style.left = '-9999px';
+        document.body.appendChild(spanElem);
+    
+        // 选择临时元素并复制
+        let range = document.createRange();
+        range.selectNode(spanElem);
+        let selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+        document.execCommand('copy');
+        selection.removeAllRanges();
+    
+        // 把临时的元素从页面中移除
+        document.body.removeChild(spanElem);
+    }
+
     // ! wait until the page is loaded
     const delay = (ms) => new Promise((r) => setTimeout(r, ms));
     const hyperLinkSelector = 'div[class="model-view--header-model-name-row"]';
@@ -71,33 +102,33 @@
     const buildsTagsSelector = 'span[class="row no-gutters justify-content-start"]';
 
     buttonContainer.append(
-        createButton('copy_id', () => {
+        createButton('copy: id', () => {
             navigator.clipboard.writeText(id);
         })
     );
 
     buttonContainer.append(
-        createButton('copy_url', () => {
+        createButton('copy: url', () => {
             navigator.clipboard.writeText(url);
         })
     );
 
     buttonContainer.append(
-        createButton('md_[model](url)', () => {
+        createButton('copy md: [model](url)', () => {
             const cont = '[model](' + url + ')';
             navigator.clipboard.writeText(cont);
         })
     );
 
     buttonContainer.append(
-        createButton('md_[model|url]', () => {
+        createButton('copy md: [model|url]', () => {
             const cont = '[model|' + url + ']';
             navigator.clipboard.writeText(cont);
         })
     );
 
     buttonContainer.append(
-        createButton('copy_last_build_id', () => {
+        createButton('copy: last_build_id', () => {
             const buildsTags = document.querySelector(buildsTagsSelector).childNodes[0].childNodes;
             const lastBuildId = buildsTags[buildsTags.length - 1].id;
             navigator.clipboard.writeText(lastBuildId);
@@ -105,7 +136,7 @@
     );
 
     buttonContainer.append(
-        createButton('copy_all_build_id', () => {
+        createButton('copy: all_build_id', () => {
             const buildsTags = document.querySelector(buildsTagsSelector).childNodes[0].childNodes;
             const buildIds = [];
             
@@ -120,34 +151,13 @@
 
     buttonContainer.append(
         createButton('hyperLink: (model)', () => {
-            // 复制hypertext再修改, 否则page上的元素也会被修改
-            let hyperlinkDup = hyperlink.cloneNode(true);
-            // 可以在这里修改 text/link, 不过这里的text都会被当成hypertext的一部分
-            hyperlinkDup.textContent = "model";
-            // hyperlinkDup.href = 'https://www.example.com';
+            generateHypertext('model', url, '(', ')');
+        })
+    );
 
-            // 创建一个新的span元素,用于包裹超链接和括号
-            var spanElem = document.createElement('span');
-            spanElem.appendChild(document.createTextNode('('));
-            spanElem.appendChild(hyperlinkDup);
-            spanElem.appendChild(document.createTextNode(')'));
-
-            // 临时将span元素插入到页面中(隐藏不可见), 这样才能选中并复制
-            spanElem.style.position = 'absolute';
-            spanElem.style.left = '-9999px';
-            document.body.appendChild(spanElem);
-
-            // 选择临时元素并复制
-            var range = document.createRange();
-            range.selectNode(spanElem);
-            var selection = window.getSelection();
-            selection.removeAllRanges();
-            selection.addRange(range);
-            document.execCommand('copy');
-            selection.removeAllRanges();
-
-            // 把临时的元素从页面中移除
-            document.body.removeChild(spanElem);
+    buttonContainer.append(
+        createButton('hyperLink: model', () => {
+            generateHypertext('model', url);
         })
     );
 
