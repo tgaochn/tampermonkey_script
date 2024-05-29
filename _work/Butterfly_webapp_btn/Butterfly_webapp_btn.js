1d9d131c5bd11e16a63e7df61935e1d0d9245079
5800X\gtfish <tgaochn@gmail.com>
2024-05-29 16:30:45 -0500

    Update Butterfly_webapp_btn version to 0.3.0 and improve layout and add text desc

--- _work/Butterfly_webapp_btn/Butterfly_webapp_btn.js
+++ _work/Butterfly_webapp_btn/Butterfly_webapp_btn.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name                Butterfly_webapp_btn
-// @version             0.2.6
+// @version             0.3
 // @description         Add btn on Butterfly webapp
 // @author              gtfish
 // @license             MIT
@@ -12,6 +12,7 @@
 // @updateURL           https://raw.githubusercontent.com/tgaochn/tampermonkey_script/master/_work/Butterfly_webapp_btn/Butterfly_webapp_btn.js
 // @downloadURL         https://raw.githubusercontent.com/tgaochn/tampermonkey_script/master/_work/Butterfly_webapp_btn/Butterfly_webapp_btn.js
 // ==/UserScript==
+// 0.3.0: improved the layout and added text desc
 // 0.2.5: reorder button positions and revise desc
 // 0.2.4: Added copy build ID and copy hypertext functionality
 // 0.2.0: 增加copy build id
@@ -45,6 +46,12 @@
         return button;
     }
 
+    function createText(text) {
+        const textElem = document.createElement('span');
+        textElem.textContent = text;
+        return textElem;
+    }
+
     function createButtonContainer() {
         const buttonContainer = document.createElement('div');
         buttonContainer.style.display = 'inline-block';
@@ -60,18 +67,18 @@
         const hyperlinkElem = document.createElement('a');
         hyperlinkElem.textContent = text;
         hyperlinkElem.href = url;
-    
+
         // 创建一个新的span元素,用于包裹超链接和括号
         const spanElem = document.createElement('span');
         spanElem.appendChild(document.createTextNode(leftPart));
         spanElem.appendChild(hyperlinkElem);
         spanElem.appendChild(document.createTextNode(rightPart));
-    
+
         // 临时将span元素插入到页面中(隐藏不可见), 这样才能选中并复制
         spanElem.style.position = 'absolute';
         spanElem.style.left = '-9999px';
         document.body.appendChild(spanElem);
-    
+
         // 选择临时元素并复制
         const range = document.createRange();
         range.selectNode(spanElem);
@@ -80,7 +87,7 @@
         selection.addRange(range);
         document.execCommand('copy');
         selection.removeAllRanges();
-    
+
         // 把临时的元素从页面中移除
         document.body.removeChild(spanElem);
     }
@@ -96,67 +103,84 @@
     }
 
     // ! add button in the container and define click func
-    const buttonContainer = createButtonContainer();
+    const buttonContainer1 = createButtonContainer();
+    const buttonContainer2 = createButtonContainer();
     const hyperlink = document.querySelector(hyperLinkSelector).childNodes[0];
     const id = hyperlink.childNodes[0].innerText;
     const url = 'https://butterfly.sandbox.indeed.net/#/model/' + id;
     const buildsTagsSelector = 'span[class="row no-gutters justify-content-start"]';
 
-    buttonContainer.append(
-        createButton('copy: id', () => {
+    buttonContainer1.append(
+        createText('text: '),
+    );
+
+    buttonContainer1.append(
+        createButton('id', () => {
             navigator.clipboard.writeText(id);
         })
     );
 
-    buttonContainer.append(
-        createButton('copy: url', () => {
+    buttonContainer1.append(
+        createButton('url', () => {
             navigator.clipboard.writeText(url);
         })
     );
 
-    buttonContainer.append(
-        createButton('copy href: (model)', () => {
+    buttonContainer1.append(
+        createText('\thref: '),
+    );
+
+    buttonContainer1.append(
+        createButton('href: (model)', () => {
             copyHypertext('model', url, '(', ')');
         })
     );
 
-    buttonContainer.append(
-        createButton('copy href: model', () => {
+    buttonContainer1.append(
+        createButton('href: model', () => {
             copyHypertext('model', url);
         })
     );
 
-    buttonContainer.append(
-        createButton('copy md: [model](url)', () => {
+    buttonContainer1.append(
+        createText('\tmd: '),
+    );
+
+    buttonContainer1.append(
+        createButton('md: [model](url)', () => {
             const cont = '[model](' + url + ')';
             navigator.clipboard.writeText(cont);
         })
     );
 
-    buttonContainer.append(
-        createButton('copy md: [model|url]', () => {
+    buttonContainer1.append(
+        createButton('md: [model|url]', () => {
             const cont = '[model|' + url + ']';
             navigator.clipboard.writeText(cont);
         })
     );
 
-    buttonContainer.append(
-        createButton('copy: last_build_id', () => {
+    buttonContainer2.append(
+        createText('builds: '),
+    );
+
+    buttonContainer2.append(
+        createButton('last_build_id', () => {
             const buildsTags = document.querySelector(buildsTagsSelector).childNodes[0].childNodes;
             const lastBuildId = buildsTags[buildsTags.length - 1].id;
             navigator.clipboard.writeText(lastBuildId);
         })
     );
 
-    buttonContainer.append(
-        createButton('copy: all_build_id', () => {
+    buttonContainer2.append(
+        createButton('all_build_id', () => {
             const buildsTags = document.querySelector(buildsTagsSelector).childNodes[0].childNodes;
             const buildIds = [];
-            
+
             buildsTags.forEach((div) => {
                 buildIds.push(div.id);
             });
-            
+
             const textToCopy = buildIds.join("\n");
             navigator.clipboard.writeText(textToCopy);
         })
@@ -165,11 +189,17 @@
     // ! add container to the table
     const table = document.querySelector('.table.table-sm.model-view--table');
     const newRow = document.createElement('tr');
-    const cell1 = document.createElement('td');
-    const cell2 = document.createElement('td');
-    cell1.textContent = 'My Btn';
-    cell2.appendChild(buttonContainer);
-    newRow.appendChild(cell1);
-    newRow.appendChild(cell2);
+    const cell11 = document.createElement('td');
+    const cell12 = document.createElement('td');
+
+    cell11.textContent = 'My Btn to copy';
+    cell12.style.display = 'flex';
+    cell12.style.flexDirection = 'column'; // contrainer 上下排列
+    // containerElement.style.flexDirection = 'row'; // contrainer 左右排列
+
+    cell12.appendChild(buttonContainer1);
+    cell12.appendChild(buttonContainer2);
+    newRow.appendChild(cell11);
+    newRow.appendChild(cell12);
     table.appendChild(newRow);
 })();
