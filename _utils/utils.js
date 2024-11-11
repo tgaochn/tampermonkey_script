a482354034bf5c25d17e259f95c7e3a1ff5a127c
5800X\gtfish <tgaochn@gmail.com>
2024-11-11 02:13:59 -0600

    重构 utils.js 文件：将模块导出改为类，新增多个按钮创建和文本节点的辅助方法

--- _utils/utils.js
+++ _utils/utils.js
@@ -1,5 +1,9 @@
-module.exports = {
-    shouldRunScript: function (inclusionPatterns, exclusionPatterns, url) {
+// utils.js
+module.exports = class Utils {
+    constructor() {
+    }
+
+    shouldRunScript(inclusionPatterns, exclusionPatterns, url) {
         // Check if the URL matches any inclusion pattern
         if (inclusionPatterns.length > 0 && !inclusionPatterns.some(pattern => pattern.test(url))) {
             return false;
@@ -12,16 +16,82 @@ module.exports = {
 
         // Default behavior for other pages
         return true;
-    },
+    }
 
-    createButtonContainer: function (document) {
+    createButtonContainer(document) {
         const container = document.createElement('div');
         container.style.display = 'inline-block';
         container.style.marginTop = '10px';
         container.style.marginLeft = '10px';
         return container;
-    },
+    }
 
+    createButton(text, callbackFunc) {
+        var button = document.createElement('button');
+        button = setBtnStyle(button);
+        button.innerHTML = text;
+        button.onclick = callbackFunc;
+        return button;
+    }
 
+    // ! btn: 普通text
+    createTextNode(text) {
+        return document.createTextNode(text);
+    }
 
+    // ! btn: 复制text
+    createButtonCopyText(text, copyText) {
+        return this.createButton(text, () => {
+            navigator.clipboard.writeText(copyText);
+        });
+    }
 };
+
+// module.exports = {
+//     // ! 额外的pattern判断是否执行脚本
+//     shouldRunScript: function (inclusionPatterns, exclusionPatterns, url) {
+//         // Check if the URL matches any inclusion pattern
+//         if (inclusionPatterns.length > 0 && !inclusionPatterns.some(pattern => pattern.test(url))) {
+//             return false;
+//         }
+
+//         // Check if the URL matches any exclusion pattern
+//         if (exclusionPatterns.length > 0 && exclusionPatterns.some(pattern => pattern.test(url))) {
+//             return false;
+//         }
+
+//         // Default behavior for other pages
+//         return true;
+//     },
+
+//     // ! 装btn的容器
+//     createButtonContainer: function (document) {
+//         const container = document.createElement('div');
+//         container.style.display = 'inline-block';
+//         container.style.marginTop = '10px';
+//         container.style.marginLeft = '10px';
+//         return container;
+//     },
+
+//     // ! btn: 通用
+//     createButton: function (text, callbackFunc) {
+//         var button = document.createElement('button');
+//         button = setBtnStyle(button);
+//         button.innerHTML = text;
+//         button.onclick = callbackFunc;
+//         return button;
+//     },
+
+//     // ! btn: 普通text
+//     createTextNode: function (text) {
+//         return document.createTextNode(text);
+//     },
+
+//     // ! btn: 复制text
+//     createButtonCopyText: function (text, copyText) {
+//         return createButton(text, () => {
+//             navigator.clipboard.writeText(copyText);
+//         });
+//     },
+
+// };
