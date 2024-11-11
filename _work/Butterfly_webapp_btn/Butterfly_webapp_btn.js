4c7b13a197fb1463013b6dd634c73555fa4088a4
5800X\gtfish <tgaochn@gmail.com>
2024-11-11 03:35:52 -0600

    更新 wiki_btn.js 文件：将版本号升级至 0.1.3，优化按钮位置逻辑并修复容器添加问题

--- _work/Butterfly_webapp_btn/Butterfly_webapp_btn.js
+++ _work/Butterfly_webapp_btn/Butterfly_webapp_btn.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name                Butterfly_webapp_btn
-// @version             0.4.7
+// @version             0.5.0
 // @description         Add btn on Butterfly webapp
 // @author              gtfish
 // @license             MIT
@@ -11,7 +11,10 @@
 // @grant               GM_registerMenuCommand
 // @updateURL           https://raw.githubusercontent.com/tgaochn/tampermonkey_script/master/_work/Butterfly_webapp_btn/Butterfly_webapp_btn.js
 // @downloadURL         https://raw.githubusercontent.com/tgaochn/tampermonkey_script/master/_work/Butterfly_webapp_btn/Butterfly_webapp_btn.js
+// @grant               GM_xmlhttpRequest
+
 // ==/UserScript==
+// 0.5.0: 重构代码, 使用外部函数
 // 0.4.7: improve the btn text
 // 0.4.5: bug fixed
 // 0.4.4: bug fixed
@@ -29,6 +32,8 @@
 
 (async function () {
     'use strict';
+    const UtilsClass = await loadExternalScript('https://raw.githubusercontent.com/tgaochn/tampermonkey_script/master/_utils/utils.js');
+    const utils = new UtilsClass();
 
     const inclusionPatterns = [
         /^https:\/\/butterfly\.sandbox\.indeed\.net\/#\/model.*$/,
@@ -37,213 +42,88 @@
     const exclusionPatterns = [
     ];
 
-    if (!shouldRunScript(inclusionPatterns, exclusionPatterns)) {
+    if (!utils.shouldRunScript(inclusionPatterns, exclusionPatterns, window.location.href)) {
         return;
     }
-
-    const observeDOM = (function () {
-        const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-        const eventListenerSupported = window.addEventListener;
-
-        return function (targetNode, onAddCallback, onRemoveCallback) {
-            if (MutationObserver) {
-                // Define a new observer
-                const mutationObserver = new MutationObserver(function (mutations, observer) {
-                    if (mutations[0].addedNodes.length && onAddCallback) {
-                        onAddCallback();
-                    }
-                });
-
-                // Have the observer observe target node for changes in children
-                mutationObserver.observe(targetNode, {
-                    childList: true,
-                    subtree: true
-                });
-            } else if (eventListenerSupported) {
-                targetNode.addEventListener('DOMNodeInserted', onAddCallback, { once: true });
-            }
-        };
-    })();
 
     // Check if the target element exists, if not, add the buttons
     const observeTarget = document.body;
     const targetElementId = "container_id";
-    observeDOM(observeTarget, () => {
+    utils.observeDOM(observeTarget, () => {
         if (!document.getElementById(targetElementId)) {
             main();
         }
     });
+
+    async function main() {
+        // ! add button in the container and define click func
+        const modelInfoButtonContainer = utils.createButtonContainer();
+        const buildInfoButtonContainer = utils.createButtonContainer();
+        const buildsTagsSelector = 'span[class="row no-gutters justify-content-start"]';
+        const modelLinkSelector = 'div[class="model-view--header-model-name-row"]';
+        const modelNameElem = document.querySelector(modelLinkSelector).childNodes[0];
+        const modelId = modelNameElem.childNodes[0].innerText;
+        const modelUrl = 'https://butterfly.sandbox.indeed.net/#/model/' + modelId;
+        const modelConfUrl = modelUrl + '/PUBLISHED/config';
+        modelInfoButtonContainer.id = "container_id";
+    
+        modelInfoButtonContainer.append(
+            utils.createTextNode('text: '),
+            utils.createButtonCopyText('id', modelId),
+            utils.createButtonCopyText('model_url', modelUrl),
+            utils.createButtonCopyText('config_url', modelConfUrl),
+    
+            utils.createTextNode('\thref: '),
+            utils.createButton('href: "model"', () => utils.copyHypertext('model', modelUrl)),
+            utils.createButton('href: {model_id}', () => utils.copyHypertext(modelId, modelUrl)),
+    
+            utils.createTextNode('\tmd: '),
+            utils.createButtonCopyText('md: ["model"](url)', `[model](${modelUrl})`),
+            utils.createButtonCopyText('md: [{model_id}](url)', `[${modelId}](${modelUrl})`),
+            // utils.createButtonCopyText('md: [model|url]', `[model|${modelUrl}]`),
+        );
+    
+        buildInfoButtonContainer.append(
+            utils.createTextNode('builds: '),
+            utils.createButton('last_build_id', () => {
+                const buildsTags = document.querySelector(buildsTagsSelector).childNodes[0].childNodes;
+                const lastBuildId = buildsTags[buildsTags.length - 1].id;
+                navigator.clipboard.writeText(lastBuildId);
+            }),
+            utils.createButton('all_build_id', () => {
+                const buildsTags = document.querySelector(buildsTagsSelector).childNodes[0].childNodes;
+                const buildIds = [];
+    
+                buildsTags.forEach((div) => {
+                    buildIds.push(div.id);
+                });
+    
+                const textToCopy = buildIds.join("\n");
+                navigator.clipboard.writeText(textToCopy);
+            }),
+    
+            utils.createTextNode('\tlinks: '),
+            utils.createButtonOpenUrl('IS promotion wiki', 'https://wiki.indeed.com/pages/viewpage.action?pageId=640792440'),
+            utils.createButtonOpenUrl('US Apply', 'https://butterfly.sandbox.indeed.net/#/proctor/jobsearch/idxbutterflyapplymodeltst?q=%24%7B%28adFormat%3D%3D%27hp%27+%7C%7C+adFormat%3D%3D%27hpd%27%29+%26%26+clientContext+%3D%3D+%27relevantJobs%27+%26%26+clientApplication+%3D%3D+%27ElephantInferenceServer%27+%26%26+country+%3D%3D+%27US%27%7D'),
+            utils.createButtonOpenUrl('US CTR', 'https://butterfly.sandbox.indeed.net/#/proctor/jobsearch/idxsjbutterflyctrmodeltst?q=%24%7B%28adFormat%3D%3D%27hp%27+%7C%7C+adFormat%3D%3D%27hpd%27%29+%26%26+clientContext+%3D%3D+%27relevantJobs%27+%26%26+clientApplication+%3D%3D+%27ElephantInferenceServer%27+%26%26+country+%3D%3D+%27US%27%7D'),
+            utils.createButtonOpenUrl('US dislike', 'https://butterfly.sandbox.indeed.net/#/proctor/jobsearch/idxbutterflydislikemodeltst'),
+        );
+    
+        // ! add container to the table
+        const table = document.querySelector('.table.table-sm.model-view--table');
+        const newRow = document.createElement('tr');
+        const cell11 = document.createElement('td');
+        const cell12 = document.createElement('td');
+    
+        cell11.textContent = 'My Btn to copy';
+        cell12.style.display = 'flex';
+        cell12.style.flexDirection = 'column'; // contrainer 上下排列
+        // containerElement.style.flexDirection = 'row'; // contrainer 左右排列
+    
+        cell12.appendChild(modelInfoButtonContainer);
+        cell12.appendChild(buildInfoButtonContainer);
+        newRow.appendChild(cell11);
+        newRow.appendChild(cell12);
+        table.appendChild(newRow);
+    }    
 })();
-
-function shouldRunScript(inclusionPatterns, exclusionPatterns) {
-    const url = window.location.href;
-
-    // Check if the URL matches any inclusion pattern
-    if (!inclusionPatterns.some(pattern => pattern.test(url))) {
-        return false;
-    }
-
-    // Check if the URL matches any exclusion pattern
-    if (exclusionPatterns.some(pattern => pattern.test(url))) {
-        return false;
-    }
-
-    // Default behavior for other pages
-    return true;
-}
-
-function main() {
-    // ! add button in the container and define click func
-    const modelInfoButtonContainer = createButtonContainer();
-    const buildInfoButtonContainer = createButtonContainer();
-    const buildsTagsSelector = 'span[class="row no-gutters justify-content-start"]';
-    const modelLinkSelector = 'div[class="model-view--header-model-name-row"]';
-    const modelNameElem = document.querySelector(modelLinkSelector).childNodes[0];
-    const modelId = modelNameElem.childNodes[0].innerText;
-    const modelUrl = 'https://butterfly.sandbox.indeed.net/#/model/' + modelId;
-    const modelConfUrl = modelUrl + '/PUBLISHED/config';
-    modelInfoButtonContainer.id = "container_id";
-
-    modelInfoButtonContainer.append(
-        createTextNode('text: '),
-        createButtonCopyText('id', modelId),
-        createButtonCopyText('model_url', modelUrl),
-        createButtonCopyText('config_url', modelConfUrl),
-
-        createTextNode('\thref: '),
-        createButton('href: "model"', () => copyHypertext('model', modelUrl)),
-        createButton('href: {model_id}', () => copyHypertext(modelId, modelUrl)),
-
-        createTextNode('\tmd: '),
-        createButtonCopyText('md: ["model"](url)', `[model](${modelUrl})`),
-        createButtonCopyText('md: [{model_id}](url)', `[${modelId}](${modelUrl})`),
-        // createButtonCopyText('md: [model|url]', `[model|${modelUrl}]`),
-    );
-
-    buildInfoButtonContainer.append(
-        createTextNode('builds: '),
-        createButton('last_build_id', () => {
-            const buildsTags = document.querySelector(buildsTagsSelector).childNodes[0].childNodes;
-            const lastBuildId = buildsTags[buildsTags.length - 1].id;
-            navigator.clipboard.writeText(lastBuildId);
-        }),
-        createButton('all_build_id', () => {
-            const buildsTags = document.querySelector(buildsTagsSelector).childNodes[0].childNodes;
-            const buildIds = [];
-
-            buildsTags.forEach((div) => {
-                buildIds.push(div.id);
-            });
-
-            const textToCopy = buildIds.join("\n");
-            navigator.clipboard.writeText(textToCopy);
-        }),
-
-        createTextNode('\tlinks: '),
-        createButtonOpenUrl('IS promotion wiki', 'https://wiki.indeed.com/pages/viewpage.action?pageId=640792440'),
-        createButtonOpenUrl('US Apply', 'https://butterfly.sandbox.indeed.net/#/proctor/jobsearch/idxbutterflyapplymodeltst?q=%24%7B%28adFormat%3D%3D%27hp%27+%7C%7C+adFormat%3D%3D%27hpd%27%29+%26%26+clientContext+%3D%3D+%27relevantJobs%27+%26%26+clientApplication+%3D%3D+%27ElephantInferenceServer%27+%26%26+country+%3D%3D+%27US%27%7D'),
-        createButtonOpenUrl('US CTR', 'https://butterfly.sandbox.indeed.net/#/proctor/jobsearch/idxsjbutterflyctrmodeltst?q=%24%7B%28adFormat%3D%3D%27hp%27+%7C%7C+adFormat%3D%3D%27hpd%27%29+%26%26+clientContext+%3D%3D+%27relevantJobs%27+%26%26+clientApplication+%3D%3D+%27ElephantInferenceServer%27+%26%26+country+%3D%3D+%27US%27%7D'),
-        createButtonOpenUrl('US dislike', 'https://butterfly.sandbox.indeed.net/#/proctor/jobsearch/idxbutterflydislikemodeltst'),
-    );
-
-    // ! add container to the table
-    const table = document.querySelector('.table.table-sm.model-view--table');
-    const newRow = document.createElement('tr');
-    const cell11 = document.createElement('td');
-    const cell12 = document.createElement('td');
-
-    cell11.textContent = 'My Btn to copy';
-    cell12.style.display = 'flex';
-    cell12.style.flexDirection = 'column'; // contrainer 上下排列
-    // containerElement.style.flexDirection = 'row'; // contrainer 左右排列
-
-    cell12.appendChild(modelInfoButtonContainer);
-    cell12.appendChild(buildInfoButtonContainer);
-    newRow.appendChild(cell11);
-    newRow.appendChild(cell12);
-    table.appendChild(newRow);
-}
-
-function setBtnStyle(btn) {
-    btn.style.backgroundColor = '#009688';
-    btn.style.color = 'white';
-    btn.style.padding = '5px 5px';
-    btn.style.height = '10px';
-    btn.style.fontSize = '14px';
-    btn.style.border = '1px solid #ccc';
-    btn.style.borderRadius = '4px';
-    btn.style.cursor = 'pointer';
-    btn.style.outline = 'none';
-    btn.style.boxSizing = 'border-box';
-}
-
-function createButton(text, callbackFunc) {
-    const useBtnStyleFromPage = true;
-    const button = document.createElement('button');
-
-    if (useBtnStyleFromPage) {
-        button.className = 'model-view--status-label badge bg-info';
-    }
-    else {
-        button.className = 'text-nowrap btn btn-warning btn-sm';
-        setBtnStyle(button);
-    }
-
-    button.innerHTML = text;
-    button.onclick = callbackFunc;
-    return button;
-}
-
-function createButtonOpenUrl(text, targetUrl) {
-    return createButton(text, () => {
-        window.open(targetUrl);
-    })
-}
-
-function createButtonCopyText(text, copyText) {
-    return createButton(text, () => {
-        navigator.clipboard.writeText(copyText);
-    });
-}
-
-function createTextNode(text) {
-    return document.createTextNode(text);
-}
-
-function createButtonContainer() {
-    const container = document.createElement('div');
-    container.style.display = 'inline-block';
-    container.style.marginTop = '10px';
-    container.style.marginLeft = '10px';
-    return container;
-}
-
-function copyHypertext(text, url, leftPart = '', rightPart = '') {
-    // Create a new anchor element
-    const hyperlinkElem = document.createElement('a');
-    hyperlinkElem.textContent = text;
-    hyperlinkElem.href = url;
-
-    // 创建一个新的span元素,用于包裹超链接和括号
-    const tempContainerElem = document.createElement('span');
-    tempContainerElem.appendChild(document.createTextNode(leftPart));
-    tempContainerElem.appendChild(hyperlinkElem);
-    tempContainerElem.appendChild(document.createTextNode(rightPart));
-
-    // 临时将span元素插入到页面中(隐藏不可见), 这样才能选中并复制
-    tempContainerElem.style.position = 'absolute';
-    tempContainerElem.style.left = '-9999px';
-    document.body.appendChild(tempContainerElem);
-
-    // 选择临时元素并复制
-    const range = document.createRange();
-    range.selectNode(tempContainerElem);
-    const selection = window.getSelection();
-    selection.removeAllRanges();
-    selection.addRange(range);
-    document.execCommand('copy');
-    selection.removeAllRanges();
-
-    // 把临时的元素从页面中移除
-    document.body.removeChild(tempContainerElem);
-}
