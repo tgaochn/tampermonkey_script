40aef9905f01c42f7813267a9797967639e4515c
5800X\gtfish <tgaochn@gmail.com>
2025-01-25 20:13:11 -0600

    更新changeTextColor.js至0.1.1，新增Butterfly proctor allocation的颜色匹配

--- changeTextColor/changeTextColor.js
+++ changeTextColor/changeTextColor.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name                text_color_changer
-// @version             0.1.0
+// @version             0.1.1
 // @description         Change text color for specific patterns using regex on specific URLs
 // @author              gtfish
 // @license             MIT
+// @match               http*://butterfly.sandbox.indeed.net/*
 // @match               http*://www.skidrowreloaded.com/*
 // @match               http*://www.amazon.com/spr/returns/*
 // @match               http*://www.mydrivers.com/zhuanti/tianti/*
@@ -13,6 +14,7 @@
 // @downloadURL         https://raw.githubusercontent.com/tgaochn/tampermonkey_script/master/changeTextColor/changeTextColor.js
 
 // ==/UserScript==
+// 0.1.1: 增加 Butterfly proctor allocation 的颜色匹配
 // 0.1.0: beta version, 优化脚本; 增加cpu/gpu天梯匹配
 // 0.0.1: init, 匹配url/text_pattern则文本修改成对应颜色. skidrow/amazon_return
 
@@ -20,6 +22,106 @@
     "use strict";
 
     const urlPatterns = {
+        Butterfly: {
+            urlRegex: /^https:\/\/butterfly\.sandbox\.indeed\.net\/#\/proctor\/jobsearch\/.*/,
+            textPatterns: [
+                // !! title colors
+                {
+                    regex: /^IDX P\(AS \| seen\)$/,
+                    textColor: "rgb(0,0,0)",
+                    backColor: "rgb(255,192,255)",
+                },
+                {
+                    regex: /^IDX Spon P\(click \| seen\)$/,
+                    textColor: "rgb(0,0,0)",
+                    backColor: "rgb(255,192,255)", 
+                },
+
+                {
+                    regex: /^IDX Spon P\(AC \| clicked\)$/,
+                    textColor: "rgb(0,0,0)",
+                    backColor: "rgb(144,238,144)",
+                },
+                {
+                    regex: /^IDX Spon P\(AS \| clicked\)$/,
+                    textColor: "rgb(0,0,0)",
+                    backColor: "rgb(144,238,144)",
+                },
+         
+                // !! allocation color
+                // HP US
+                {
+                    regex: /^((RJP HP)|(HP)|(RJP)) US w\/ IS$/,
+                    textColor: "rgb(0,0,0)",
+                    backColor: "rgb(255,228,181)",
+                },
+                {
+                    regex: /^((RJP HP)|(HP)|(RJP)) US w\/ IS Shadow Traffic$/,
+                    textColor: "rgb(0,0,0)",
+                    backColor: "rgb(255,225,225)",
+                },
+         
+                // HP ROW
+                {
+                    regex: /^((RJP HP)|(HP)|(RJP)) (ROW)|(ROTW) w\/ IS$/,
+                    textColor: "rgb(0,0,0)",
+                    backColor: "rgb(255,218,171)",
+                },
+                {
+                    regex: /^((RJP HP)|(HP)|(RJP)) (ROW)|(ROTW) w\/ IS Shadow Traffic$/,
+                    textColor: "rgb(0,0,0)",
+                    backColor: "rgb(255,225,225)",
+                },
+         
+                // HP JP
+                {
+                    regex: /^((RJP HP)|(HP)|(RJP)) JP w\/ IS$/,
+                    textColor: "rgb(0,0,0)",
+                    backColor: "rgb(255,208,161)",
+                },
+                {
+                    regex: /^((RJP HP)|(HP)|(RJP)) JP w\/ IS Shadow Traffic$/,
+                    textColor: "rgb(0,0,0)",
+                    backColor: "rgb(255,225,225)",
+                },
+         
+                // SERP US
+                {
+                    regex: /^SERP US w\/ IS$/,
+                    textColor: "rgb(0,0,0)",
+                    backColor: "rgb(255,182,193)",
+                },
+                {
+                    regex: /^SERP US w\/ IS Shadow Traffic$/,
+                    textColor: "rgb(0,0,0)",
+                    backColor: "rgb(255,225,225)",
+                },
+         
+                // SERP ROW
+                {
+                    regex: /^SERP (ROW)|(ROTW) w\/ IS$/,
+                    textColor: "rgb(0,0,0)",
+                    backColor: "rgb(255,172,183)",
+                },
+                {
+                    regex: /^SERP (ROW)|(ROTW) w\/ IS Shadow Traffic$/,
+                    textColor: "rgb(0,0,0)",
+                    backColor: "rgb(255,225,225)",
+                },
+         
+                // SERP JP
+                {
+                    regex: /^SERP JP w\/ IS$/,
+                    textColor: "rgb(0,0,0)",
+                    backColor: "rgb(255,162,173)",
+                },
+                {
+                    regex: /^SERP JP w\/ IS Shadow Traffic$/,
+                    textColor: "rgb(0,0,0)",
+                    backColor: "rgb(255,225,225)",
+                },
+            ],
+         },        
         skidrow: {
             urlRegex: /^https?:\/\/www\.skidrowreloaded\.com\/.*/,
             textPatterns: [
