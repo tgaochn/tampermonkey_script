9903744d4e8270c10fb7db40307ff2df335d7466
5800X\gtfish <tgaochn@gmail.com>
2025-03-04 23:15:15 -0600

    更新 Claude_add_buttons.js 至 0.6.6，修改按钮提示和增加新的 Slack 回复优化功能

--- LLM_add_buttons/Claude_add_buttons.js
+++ LLM_add_buttons/Claude_add_buttons.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Claude_Add_Buttons
 // @namespace   https://claude.ai/
-// @version     0.6.5
+// @version     0.6.6
 // @description Adds buttons for Claude
 // @author      gtfish
 // @match       https://claude.ai/*
@@ -11,6 +11,7 @@
 // @updateURL       https://raw.githubusercontent.com/tgaochn/tampermonkey_script/master/LLM_add_buttons/Claude_add_buttons.js
 // @downloadURL     https://raw.githubusercontent.com/tgaochn/tampermonkey_script/master/LLM_add_buttons/Claude_add_buttons.js
 // ==/UserScript==
+// Claude_Add_Buttons 0.6.6: 修改prompt
 // Claude_Add_Buttons 0.6.5: 部分重构代码, 用于适配 deepseek
 // Claude_Add_Buttons 0.6.4: 增加 prompt; 增加了追加内容的模式
 // Claude_Add_Buttons 0.6.3: 增加 prompt
@@ -56,15 +57,15 @@
             prompt: "Reformat the response in the format of raw markdown code (markdown code wrapped in triple backticks) so I can copy and paste into my markdown editor.",
         },
         example_: {
-            btnNm: "加例子⏎",
+            btnNm: "加例子解释⏎",
             sendOutPrompt: true,
-            prompt: "give me more examples to explain the response. The examples should be easy to understand and explain why the example supports the response if applicable.",
+            prompt: "Make the response more understandable and give me more examples to explain the response if applicable. The examples should be easy to understand and explain why the example supports the response if applicable.",
         },
-        rewrite_: {
-            btnNm: "改写⏎",
-            sendOutPrompt: true,
-            prompt: "Rewrite the response and make the response more understandable.",
-        },
+        // rewrite_: {
+        //     btnNm: "改写⏎",
+        //     sendOutPrompt: true,
+        //     prompt: "Rewrite the response and make the response more understandable.",
+        // },
     };
 
     // 不带下划线的prompt会在输入框中显示并等待继续输入内容
@@ -84,87 +85,10 @@
 1. The text will be used in discussion on slack btw colleagues.\n
 2. Please respond in the format of raw markdown code (markdown code wrapped in triple backticks), so I can copy and paste it into a markdown editor.\n
 `,
-            // 3. The revised text should be simple and easy to understand.\n
-        },
-        explain_translate: {
-            btnNm: "日常-解释翻译",
-            sendOutPrompt: false,
-            prompt: `I'm not a native English speaker, and I cannot fully understand the following content. Could you explain what it means and what it may possibly imply so that it can be easily understood? More background or requirements of the text are shown below. They should be applied to all the follow-up responses.\n
-1. The response should be in Chinese, but the comments in the code block should be in English if applicable.\n
-2. The text is from a discussion on Slack between American colleagues or documentation of some projects. The company is high-tech, such as Google, and the colleagues are managers, data scientists, or machine learning engineers on Recsys.\n
-3. Please have a separate section to explain all the points that may cause confusing in the background of ML/AI/Recsys.\n
-4. If it is a question, explain the background and the main points. Then provide a possible answer.\n
-5. If it is a request, explain the main points and the objective. Then provide a possible answer.\n
-6. If it is a suggestion, explain the main points, the objective, and the reasons.\n
-The text is:`,
-        },
-        summarize: {
-            btnNm: "日常-总结",
-            sendOutPrompt: false,
-            prompt: "Summarize the following text in both English and Chinese in a paragraph then reformat it in some bullets. Please respond in the format of raw markdown code (markdown code wrapped in triple backticks), so I can copy and paste it into a markdown editor.\n",
-        },
-        chn2eng: {
-            btnNm: "日常-中翻英",
-            sendOutPrompt: false,
-            prompt: "translate the following Chinese text into English in different tones, which will be used in messages between colleagues and formal emails: \n",
-        },
-        ocr: {
-            btnNm: "img_OCR",
-            sendOutPrompt: false,
-            prompt: `Please OCR the attached image following these backgrounds and instructions:\n
-1. You need to act as a very senior machine learning engineer in an OCR software developing company.\n
-2. The task is to identify the content, same as what OCR software does.\n
-3. The content could be a piece of code, some plain text, or a table. \n
-4. Please also check whether sentence or words the OCR results are reasonable. If there are any issues due to inaccurate OCR results, please fix them.\n
-5. Please follow these instructions in all the responses in this session for the further questions.\n
-6. Take a deep breath and work on this problem step-by-step.\n
-7. Please respond in the format of raw markdown code (markdown code wrapped in triple backticks), so I can copy and paste it into a markdown editor.\n
-`,
-        },
-        fix_ocr: {
-            btnNm: "fix_cont_OCR",
-            sendOutPrompt: false,
-            prompt: `Response based on the given content obtained from OCR software following these backgrounds and instructions:\n
-1. You need to act as a very senior machine learning engineer in an OCR software developing company.\n
-2. The task is to manually improve the raw results from OCR software.\n
-3. The content could be a piece of code, some plain text or a table. \n
-4. Please follow these instructions in all the responses in this session for the further questions.\n
-5. Take a deep breath and work on this problem step-by-step.\n
-6. If applicable, the response should be in the format of raw markdown code so I can copy and paste into my markdown editor.\n
-7. Please respond in the format of raw markdown code (markdown code wrapped in triple backticks), so I can copy and paste it into a markdown editor.\n
-It may include some errors or formatting issues due to inaccurate OCR results. You need to fix these issues and make it as readable and explainable as possible. Also, you need to have a brief explanation of the content.\n
-`,
-        },
-
-        format_tex_formula: {
-            btnNm: "format tex formula",
-            sendOutPrompt: false,
-            prompt: `Could you format the following tex formula and make it more readable?\n\n 
-Please provide the response following these backgrounds and instructions:\n
-1. You need to act as a senior latex expert and a senior machine learning engineer.\n
-2. The overall purpose of the revision is to make the formula more readable so the reader of the latex code can easily understand it.\n
-3. Please respond in the format of raw markdown code (markdown code wrapped in triple backticks), so I can copy and paste it into a markdown editor.\n
-4. The latex code for the formula is from OCR, so it may include errors. If there are errors, please correct them and explain the changes in detail.\n
-5. Please follow these instructions in all the responses in this session for further questions.\n
-6. Take a deep breath and work on this problem step-by-step.\n
-`,
-        },
-
-        online_debate: {
-            btnNm: "网上吵架",
-            sendOutPrompt: false,
-            prompt: `请你作为一位资深的网络辩论专家，精通分析对方观点中的漏洞并激怒对方. 帮我分析并改进以下网络辩论中的回复。我的一部分目的是激怒对方, 所以请不要在意语气和态度. 可以使用反讽、挖苦等语言技巧. 重点是论据的锐利性和对方情绪的操控.\n
-1. 如果不改变我回复的大致结构, 请分析我的回复中有什么逻辑漏洞. 对方之前的论述中有什么我遗漏的弱点可以利用. 以及对方的情绪触发点在哪里, 如何让对方失去理性陷入情绪化. 另外请预判对方可能的反驳并给出后续应对策略.\n
-2. 如果完全改变我回复的结构, 请给出新的回复建议并说明理由. 另外请预判对方可能的反驳并给出后续应对策略\n
-争论背景：\n
-\n\n
-我的预计回复：\n
-\n\n
-`,
         },
 
         reply_on_slack: {
-            btnNm: "slack 回复优化",
+            btnNm: "slack 完整回复优化",
             sendOutPrompt: false,
             prompt: `I'm reviewing a Slack discussion between me (Tian Gao) and my colleagues at high-tech company like google or amazon. As a non-native English speaker, I'd like to ensure my planned reply is clear and appropriate. Could you help analyze my draft response, check for any misunderstandings of the discussion context, and suggest improvements?\n
 Requirements:\n
@@ -182,6 +106,97 @@ My draft reply:\n
 \n\n
 `,
         },
+
+        explain_translate: {
+            btnNm: "日常-解释翻译",
+            sendOutPrompt: false,
+            prompt: `I'm not a native English speaker, and I cannot fully understand the following content. Could you explain what it means and what it may possibly imply so that it can be easily understood? More background or requirements of the text are shown below. They should be applied to all the follow-up responses.\n
+1. The response should be in Chinese, but the comments in the code block should be in English if applicable.\n
+2. The text is from a discussion on Slack between American colleagues or documentation of some projects. The company is high-tech, such as Google, and the colleagues are managers, data scientists, or machine learning engineers on Recsys.\n
+3. Please have a separate section to explain all the points that may cause confusing in the background of ML/AI/Recsys.\n
+4. If it is a question, explain the background and the main points. Then provide a possible answer.\n
+5. If it is a request, explain the main points and the objective. Then provide a possible answer.\n
+6. If it is a suggestion, explain the main points, the objective, and the reasons.\n
+The text is:`,
+        },
+        summarize: {
+            btnNm: "日常-总结",
+            sendOutPrompt: false,
+            prompt: "Summarize the following text in both English and Chinese in a paragraph then reformat it in some bullets. The text can be in the format of subtitle, plaintext or others. Please respond in the format of raw markdown code (markdown code wrapped in triple backticks), so I can copy and paste it into a markdown editor.\n",
+        },
+
+        chn2eng: {
+            btnNm: "日常-中翻英",
+            sendOutPrompt: false,
+            prompt: "translate the following Chinese text into English in different tones, which will be used in messages between colleagues and formal emails: \n",
+        },
+
+        ocr: {
+            btnNm: "img_OCR",
+            sendOutPrompt: false,
+            prompt: `Please OCR the attached image following these backgrounds and instructions:\n
+1. You need to act as a very senior machine learning engineer in an OCR software developing company.\n
+2. The task is to identify the content, same as what OCR software does.\n
+3. The content could be a piece of code, some plain text, or a table. \n
+4. Please also check whether sentence or words the OCR results are reasonable. If there are any issues due to inaccurate OCR results, please fix them.\n
+5. Please follow these instructions in all the responses in this session for the further questions.\n
+6. Please respond in the format of raw markdown code (markdown code wrapped in triple backticks), so I can copy and paste it into a markdown editor.\n
+`,
+        },
+
+        fix_ocr: {
+            btnNm: "fix_cont_OCR",
+            sendOutPrompt: false,
+            prompt: `Response based on the given content obtained from OCR software following these backgrounds and instructions:\n
+1. You need to act as a very senior machine learning engineer in an OCR software developing company.\n
+2. The task is to manually improve the raw results from OCR software.\n
+3. The content could be a piece of code, some plain text or a table. \n
+4. Please follow these instructions in all the responses in this session for the further questions.\n
+5. Take a deep breath and work on this problem step-by-step.\n
+6. If applicable, the response should be in the format of raw markdown code so I can copy and paste into my markdown editor.\n
+7. Please respond in the format of raw markdown code (markdown code wrapped in triple backticks), so I can copy and paste it into a markdown editor.\n
+It may include some errors or formatting issues due to inaccurate OCR results. You need to fix these issues and make it as readable and explainable as possible. Also, you need to have a brief explanation of the content.\n
+`,
+        },
+
+        mermaid_ocr: {
+            btnNm: "mermaid_OCR",
+            sendOutPrompt: false,
+            prompt: `Please transform the attached flowchart into mermaid code following these backgrounds and instructions:\n
+1. You need to act as a very senior machine learning engineer and a mermaid expert in an OCR software developing company.\n
+2. Check whether sentence or words in the flowchart are reasonable. If there are any issues due to inaccurate OCR results, please fix them and list the changes.\n
+3. Follow these instructions in all the responses in this session for the further questions.\n
+4. Respond in the format of raw markdown code (\`\`\`mermaid\n\`\`\`), so I can copy and paste it into a markdown editor. Don't response with raw mermaid code.\n
+5. Use "classDef defaultNode fill:#f9f9f9,stroke:#0000ff,stroke-width:2px,color:black" as the default style for all the nodes.\n
+`,
+        },
+
+//         format_tex_formula: {
+//             btnNm: "format tex formula",
+//             sendOutPrompt: false,
+//             prompt: `Could you format the following tex formula and make it more readable?\n\n 
+// Please provide the response following these backgrounds and instructions:\n
+// 1. You need to act as a senior latex expert and a senior machine learning engineer.\n
+// 2. The overall purpose of the revision is to make the formula more readable so the reader of the latex code can easily understand it.\n
+// 3. Please respond in the format of raw markdown code (markdown code wrapped in triple backticks), so I can copy and paste it into a markdown editor.\n
+// 4. The latex code for the formula is from OCR, so it may include errors. If there are errors, please correct them and explain the changes in detail.\n
+// 5. Please follow these instructions in all the responses in this session for further questions.\n
+// 6. Take a deep breath and work on this problem step-by-step.\n
+// `,
+//         },
+
+        //         online_debate: {
+        //             btnNm: "网上吵架",
+        //             sendOutPrompt: false,
+        //             prompt: `请你作为一位资深的网络辩论专家，精通分析对方观点中的漏洞并激怒对方. 帮我分析并改进以下网络辩论中的回复。我的一部分目的是激怒对方, 所以请不要在意语气和态度. 可以使用反讽、挖苦等语言技巧. 重点是论据的锐利性和对方情绪的操控.\n
+        // 1. 如果不改变我回复的大致结构, 请分析我的回复中有什么逻辑漏洞. 对方之前的论述中有什么我遗漏的弱点可以利用. 以及对方的情绪触发点在哪里, 如何让对方失去理性陷入情绪化. 另外请预判对方可能的反驳并给出后续应对策略.\n
+        // 2. 如果完全改变我回复的结构, 请给出新的回复建议并说明理由. 另外请预判对方可能的反驳并给出后续应对策略\n
+        // 争论背景：\n
+        // \n\n
+        // 我的预计回复：\n
+        // \n\n
+        // `,
+        //         },
     };
 
     const myPromptJson3 = {
@@ -357,7 +372,7 @@ Give me a detailed response following these backgrounds and instructions:\n
             const btnSubContainer1 = utils.createButtonContainerFromJson(inputBoxElement, myPromptJson1);
             const btnSubContainer2 = utils.createButtonContainerFromJson(inputBoxElement, myPromptJson2);
             const btnSubContainer3 = utils.createButtonContainerFromJson(inputBoxElement, myPromptJson3);
-            const btnSubContainer4 = utils.createButtonContainerFromJson(inputBoxElement, myPromptJson4, 'append');
+            const btnSubContainer4 = utils.createButtonContainerFromJson(inputBoxElement, myPromptJson4, "append");
 
             btnSubContainer1.id = addedContainerId;
 
